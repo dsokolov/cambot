276f3dcaae174108f13318efed1f0103baf1f5ec
Dmitry I. Sokolov <dmitryilichsokolov@gmail.com>
2019-12-17 15:39:41 +0400

    Add proxy support

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
+### Misc
+
 sudo chmod a+rwx /var/run/docker.sock
 sudo chmod a+rwx /var/run/docker.pid
 
+### Build
+
 docker build . -t cambot
+
+### Save container
+```
+docker save --output cambot.tar cambot
+```
+
+### Load container
+```
+docker load --input cambot.tar
+```
+
 
 ### Run
 
 ```
 docker run --device=/dev/video0:/dev/video0 cambot
 ```
+
--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
+import configparser
 import datetime
 
 import cv2
 from telegram.ext import Updater, CommandHandler
-import configparser
-import io
 
 
 def hello(update, context):
@@ -29,11 +28,20 @@ if __name__ == "__main__":
 
     config = configparser.RawConfigParser()
     config.read('bot.properties')
-    TOKEN = config.get('Bot', 'api.token')
+    TOKEN = config.get('Bot', 'token')
+    use_proxy = config.getboolean('Bot', 'use_proxy')
+    PROXY = config.get('Bot', 'proxy')
+    REQUEST_KWARGS = {
+        'proxy_url': PROXY,
+    }
 
     frame = None
     cap = cv2.VideoCapture(0)
-    updater = Updater(TOKEN, use_context=True)
+    if use_proxy:
+        print("Use proxy {}".format(PROXY))
+        updater = Updater(TOKEN, use_context=True, request_kwargs=REQUEST_KWARGS)
+    else:
+        updater = Updater(TOKEN, use_context=True)
     updater.dispatcher.add_handler(CommandHandler('hello', hello))
     updater.dispatcher.add_handler(CommandHandler('cam', cam))
     updater.start_polling()
